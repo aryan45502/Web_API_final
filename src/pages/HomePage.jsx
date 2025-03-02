839d526aef9c209a8f603c66750f49a9ede2a883
aryan45502 <aryanghimire671@gmail.com>
2025-03-02 20:29:49 +0545

    homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div>
+        <h1>Welcome to Event Planner</h1>
+        <p>Plan your perfect wedding with us.</p>
+    </div>
+);
+
+export default HomePage;
